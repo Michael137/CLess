31589fa0c1bd9624808d340ecf9d11e57ed4e9b7
Michael Buch <michaelbuch12@gmail.com>
2018-09-06 12:03:52 +0200

    use constants

--- curses/board.c
+++ curses/board.c
@@ -1,18 +1,19 @@
+#include "constants.h"
 #include <ncurses.h>
 #include <stdbool.h>
 
 void draw_board_border( WINDOW** board )
 {
 	wborder( *board, '|', '|', '-', '-', '+', '+', '+', '+' );
-	int y, x, maxy, maxx;
-	getyx( *board, y, x );
-	getmaxyx( *board, maxy, maxx );
+	// int y, x, maxy, maxx;
+	// getyx( *board, y, x );
+	// getmaxyx( *board, maxy, maxx );
 
-	for( x += 1; x < maxx - 4; x += 4 )
-		mvwprintw( *board, maxy - 1, x, "| %c |", x / 4 + 'a' );
+	// for( x += 1; x < maxx - 4; x += 4 )
+	//	mvwprintw( *board, maxy - 1, x, "| %c |", x / 4 + 'a' );
 
-	for( y += 1; y < maxy - 2; y += 2 )
-		mvwprintw( *board, y, 0, "%d", y / 2 );
+	// for( y += 1; y < maxy - 2; y += 2 )
+	//	mvwprintw( *board, y, 0, "%d", y / 2 );
 
 	wrefresh( *board );
 	wmove( *board, 0, 0 );
@@ -35,18 +36,20 @@ void fill_board( WINDOW** board )
 
 	int x, y;
 	int x_offset = 0;
-	int color_ctr = 2;
-	for( y = starty + 1; y < maxy - 1; y += 2, color_ctr++ ) {
-		for( x = startx + x_offset + 2; x < maxx - 2; x += 8 ) {
-			mvwchgat( *board, y, x, 4, A_REVERSE, 0, NULL );
-			mvwchgat( *board, y + 1, x, 4, A_REVERSE, 0, NULL );
+	bool with_offset = false;
+	for( y = starty; y < maxy; y += SQUARE_HEIGHT ) {
+		for( x = startx + x_offset; x < maxx; x += SQUARE_WIDTH * 2 ) {
+			for( int i = 0; i < SQUARE_HEIGHT; ++i )
+				mvwchgat( *board, y + i, x, SQUARE_WIDTH, A_REVERSE, 0, NULL );
 		}
 
-		if( color_ctr == 2 ) {
-			x_offset = 4;
-			color_ctr = 0;
-		} else
+		if( !with_offset ) {
+			x_offset = SQUARE_WIDTH;
+			with_offset = true;
+		} else {
 			x_offset = 0;
+			with_offset = false;
+		}
 	}
 
 	wrefresh( *board );
@@ -62,8 +65,8 @@ int main()
 	cbreak();
 	keypad( stdscr, TRUE );
 
-	height = 18;
-	width = 36;
+	height = BOARD_HEIGHT;
+	width = BOARD_WIDTH;
 	starty = ( LINES - height ) / 2;
 	startx = ( COLS - width ) / 2;
 	refresh();
