98edc2dc0aae57636d4b15a51d49d7b8bac769ea
Michael Buch <michaelbuch12@gmail.com>
2018-09-13 18:27:09 +0200

    makefile cleanup; more tests

--- antlr/RookParser.g4
+++ antlr/RookParser.g4
@@ -7,6 +7,6 @@ options {
 /*
  * Parser rules
  */
-game		:		line+ EOF ;
-line		:		COMMENT* MOVE_NUM_LABEL (SCORE | move) (SCORE | move) NEWLINE ;
+game		:		NEWLINE* line+ EOF ;
+line		:		((COMMENT+) | COMMENT* MOVE_NUM_LABEL (SCORE | move) (SCORE | move) NEWLINE*) ;
 move		:		((((PIECE? (ALPH_IDX? CAPTURE )? CELL) | CASTLE_SHORT | CASTLE_LONG | PROMOTION) (CHECK|MATE)?) | WHITESPACE+ | COMMENT+ ) ;
--- antlr/main.cpp
+++ antlr/main.cpp
@@ -5,6 +5,30 @@
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+// std::string stream( "1. e4 e5\n2. Nf3 Nc6\n3. Bb5 a6\n4. Bxc6 dxc6\n5. O-O 1-0 (black resigns)\n\n" );
+//	std::string stream( R"(
+//1. e4 e5
+//2. Nf3 Nc6
+//3. Bb5 a6
+//4. Bxc6 dxc6
+//5. O-O 1-0 (black resigns)
+//)" );
+	std::ifstream stream("test/test_no_newlines.in");
+
+	antlr4::ANTLRInputStream input(stream);
+
+	rook::RookLexer lexer( &input );
+	antlr4::CommonTokenStream tokens( &lexer );
+
+	tokens.fill();
+	for( auto token : tokens.getTokens() ) {
+		std::cout << token->toString() << std::endl;
+	}
+
+	rook::RookParser parser( &tokens );
+	antlr4::tree::ParseTree* tree = parser.game();
+
+	std::cout << tree->toStringTree( &parser ) << std::endl;
+
 	return 0;
 }
